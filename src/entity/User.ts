e8382bc7d565e578c895b9480f7e6c08700dacd0
gallaguz <gallaguz@gmail.com>
2021-07-02 04:52:04 +0300

    uuid

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,16 +1,39 @@
-import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm'
+import {
+	Entity,
+	PrimaryGeneratedColumn,
+	Column,
+	BaseEntity,
+	CreateDateColumn,
+	UpdateDateColumn,
+	BeforeInsert,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
 
 @Entity('users')
 export class User extends BaseEntity {
 	@PrimaryGeneratedColumn()
-	id: number
+	id: number;
 
 	@Column()
-	name: string
+	name: string;
 
 	@Column()
-	email: string
+	email: string;
 
 	@Column()
-	role: string
+	role: string;
+
+	@Column({ type: 'uuid' })
+	uuid: string;
+
+	@CreateDateColumn()
+	createdAt: Date;
+
+	@UpdateDateColumn()
+	updatedAt: Date;
+
+	@BeforeInsert()
+	createUUid() {
+		this.uuid = uuid();
+	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,52 @@
 import 'reflect-metadata';
-import {createConnection} from 'typeorm';
-import express, {Request, Response} from 'express';
+import { createConnection } from 'typeorm';
+import express, { Request, Response } from 'express';
 
-
-import {User} from './entity/User';
-
+import { User } from './entity/User';
 
 const app = express();
 app.use(express.json());
 
 // CREATE
 app.post('/users', async (req: Request, res: Response) => {
-  const {name, email, role} = req.body;
+	const { name, email, role } = req.body;
+
+	try {
+		const user = User.create({ name, email, role });
+
+		await user.save();
+
+		return res.status(201).json(user);
+	} catch (e) {
+		console.log(e);
+
+		return res.status(500).json(e);
+	}
 });
 
 // READ
+app.get('/users', async (_: Request, res: Response) => {
+	try {
+		const users = await User.find();
 
-// Update
+		return res.status(200).json(users);
+	} catch (e) {
+		console.log(e);
 
-// Delete
+		return res.status(500).json(e);
+	}
+});
 
-// Find
+// UPDATE
 
-createConnection().then(async () => {
-  app.listen(5000, () => {
-    console.log('server start at http://localhost:5000');
-  });
-}).catch((error) => console.log(error));
+// DELETE
+
+// FIND
+
+createConnection()
+	.then(async () => {
+		app.listen(5000, () => {
+			console.log('server start at http://localhost:5000');
+		});
+	})
+	.catch((error) => console.log(error));
